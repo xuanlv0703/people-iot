2b98858a1d3cbae6e3d65f6085363a52e50aafd5
Hieu Huynh <hieu@rasia.info>
2016-09-12 17:46:23 +0700

    .

--- app/components/peopleIOT/peopleIOTController.js
+++ app/components/peopleIOT/peopleIOTController.js
@@ -230,7 +230,7 @@ app.controller('peopleIOTCtrl', ['$scope', 'ConfigService', '$http', '$filter', 
         };
 
         // var socket = io.connect();
-        var socket = io.connect("http://people-rutledge.apps.openshift.rasia:8000");
+        var socket = io.connect("http://people-rutledge.apps.openshift.rasia:8080");
         var x = undefined;
         var y = undefined;
         var id = undefined;
