89f349df734d9740a2982b599e04c52aabf3cfc7
Xuan Le <levanxuan@gmail.com>
2016-09-15 09:58:24 +0700

    Update peopleIOTController.js

--- app/components/peopleIOT/peopleIOTController.js
+++ app/components/peopleIOT/peopleIOTController.js
@@ -229,7 +229,7 @@ app.controller('peopleIOTCtrl', ['$scope', 'ConfigService', '$http', '$filter', 
             }
         };
 
-        var socket = io('http://people-rutledge.apps.openshift.rasia');
+        var socket = io('hpeople-rutledge.apps.openshift.rasia/#/');
         console.log(socket);
         var x = undefined;
         var y = undefined;
